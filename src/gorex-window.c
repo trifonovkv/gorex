14feddb8b1b4d5399a968f07003850d1f51dd7f1
trifonovkv <trifonovkv@gmail.com>
2015-08-18 11:41:21 +0500

    Added GtkHeaderBar

--- src/gorex-window.c
+++ src/gorex-window.c
@@ -16,19 +16,22 @@ struct _GorexWindowClass
 G_DEFINE_TYPE (GorexWindow, gorex_window, GTK_TYPE_APPLICATION_WINDOW);
 
 static void
-gorex_window_init (GorexWindow *app)
+gorex_window_init (GorexWindow *window)
 {
+	gtk_widget_init_template (GTK_WIDGET (window));
 }
 
 static void
 gorex_window_class_init (GorexWindowClass *class)
 {
+	gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
+                                					"/org/gnome/gorex/gorex-window.ui");
 }
 
 GorexWindow *
-gorex_window_new (GorexApplication *app)
+gorex_window_new (GorexApplication *application)
 {
-  return g_object_new (GOREX_TYPE_WINDOW, "application", app, NULL);
+  return g_object_new (GOREX_TYPE_WINDOW, "application", application, NULL);
 }
 
 void
