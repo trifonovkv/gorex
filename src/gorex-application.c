201c73062bb75d0061f9d03b1c4a4b29aa74511f
trifonovkv <trifonovkv@gmail.com>
2015-08-22 13:02:50 +0500

    Added application menu

--- src/gorex-application.c
+++ src/gorex-application.c
@@ -21,6 +21,55 @@ gorex_application_init (GorexApplication *app)
 }
 
 static void
+open_file_activated (GSimpleAction *action,
+                     GVariant      *parameter,
+                     gpointer       app)
+{
+}
+
+static void
+quit_activated (GSimpleAction *action,
+                GVariant      *parameter,
+                gpointer       app)
+{
+    g_application_quit (G_APPLICATION (app));
+}
+
+static GActionEntry app_entries[] =
+{
+    { "open_file", open_file_activated, NULL, NULL, NULL },
+    { "quit", quit_activated, NULL, NULL, NULL }
+};
+
+static void
+gorex_application_startup (GApplication *app)
+{
+    GtkBuilder *builder;
+    GMenuModel *app_menu;
+    const gchar *open_accels[2] = { "<Ctrl>O", NULL };
+    const gchar *quit_accels[2] = { "<Ctrl>Q", NULL };
+
+    G_APPLICATION_CLASS (gorex_application_parent_class)->startup (app);
+
+    g_action_map_add_action_entries (G_ACTION_MAP (app),
+                                     app_entries, G_N_ELEMENTS (app_entries),
+                                     app);
+    gtk_application_set_accels_for_action (GTK_APPLICATION (app),
+                                           "app.quit",
+                                           quit_accels);
+    gtk_application_set_accels_for_action (GTK_APPLICATION (app),
+                                           "app.open_file",
+                                           open_accels);
+
+
+    builder = gtk_builder_new_from_resource (
+                                        "/org/gnome/gorex/gorex-app-menu.ui");
+    app_menu = G_MENU_MODEL (gtk_builder_get_object (builder, "appmenu"));
+    gtk_application_set_app_menu (GTK_APPLICATION (app), app_menu);
+    g_object_unref (builder);
+}
+
+static void
 gorex_application_activate (GApplication *app)
 {
     GorexWindow *win;
@@ -56,6 +105,7 @@ gorex_application_class_init (GorexApplicationClass *class)
 {
     G_APPLICATION_CLASS (class)->activate = gorex_application_activate;
     G_APPLICATION_CLASS (class)->open = gorex_application_open;
+    G_APPLICATION_CLASS (class)->startup = gorex_application_startup;
 }
 
 GorexApplication *
